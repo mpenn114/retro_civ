abfd2da83f57b92895c52a0ec8bdae28ec1bcc04
matt-penn-fa <matt.penn2@thefa.com>
2026-07-29 09:59:13 +0100

    feat: add initial app setup

---
+++ src/run_game.py
@@ -0,0 +1,9 @@
+def run_game():
+    """
+    Run the game
+    """
+    print("Game running")
+
+
+if __name__ == "__main__":
+    run_game()
---
+++ tests/test_run_game.py
@@ -0,0 +1,5 @@
+from src.run_game import run_game
+
+
+def test_run_game():
+    run_game()
